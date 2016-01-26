f1b64cd0f66c96b3018ce1e601a00556f0588d87
Forneet <forneet@gmail.com>
2016-01-26 16:58:38 +0200

    fin soap, xml

--- demo/soap/client.php
+++ demo/soap/client.php
@@ -1,12 +1,13 @@
 <?php
 try {
   // Создание SOAP-клиента
-  $client = new SoapClient("http://mysite.local/demo/soap/stock.wsdl");
-	
+  $client = new SoapClient("http://localhost/spec/spec/soap/stock.wsdl");
+	print_r( $client->__getFunctions());
+//exit;
   // Посылка SOAP-запроса c получением результат
-  $result = $client->getStock("7");
+  $result = $client->getStock("1");
   echo "Текущий запас на складе: ", $result;
 } catch (SoapFault $exception) {
-  echo $exception->getMessage();	
+  echo $exception->getMessage();
 }
 ?>
--- demo/soap/server.php
+++ demo/soap/server.php
@@ -1,13 +1,15 @@
 <?php
+
+class StockService{
 // Описание функции Web-сервиса
   function getStock($id) {	
-    $stock = array(
+    $stock = [
 			"1" => 100,
 			"2" => 200,
 			"3" => 300,
 			"4" => 400,
 			"5" => 500
-    );
+    ];
     if (isset($stock[$id])) {
       $quantity = $stock[$id];		
       return $quantity;
@@ -15,12 +17,14 @@
       throw new SoapFault("Server", "Несуществующий id товара");
     }	
   }
+}
 // Отключение кэширования WSDL-документа
 ini_set("soap.wsdl_cache_enabled", "0");
 // Создание SOAP-сервер
-$server = new SoapServer("http://mysite.local/demo/soap/stock.wsdl");
+$server = new SoapServer("http://localhost/spec/spec/demo/soap/stock.wsdl");
 // Добавить класс к серверу
-$server->addFunction("getStock");
+//$server->addFunction("getStock");
+$server->setClass("StockService");
 // Запуск сервера
 $server->handle();
 ?>
